b9457c97f1d6a09efb5ed91c264a1e489b6b77ea
aquerinj <arnaud.querinjean@ulb.be>
2023-03-06 10:59:57 +0100

    Copie du code principal dans ma branche

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+package ulb.info307.g6;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class of the app.
+ */
+public class Main extends Application {
+    /**
+     * Launches an empty javaFX window.
+     */
+    @Override
+    public  void start(Stage window) {
+        window.setTitle("Projet 2023-groupe-6");
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
