8f74cea50c43612f7ce29b3abd58756ca1b95d53
Arthur Coenen <45007111+arthurco@users.noreply.github.com>
2023-03-07 23:06:35 +0100

    H2-T3 : this file is only there for manual interaction/testing with the database as there is no DB browser.
    Co-authored-by: <Simon.Peetroons@ulb.be>

--- src/ulb/info307/g6/MainDB.java
+++ src/ulb/info307/g6/MainDB.java
@@ -1,34 +1,20 @@
 package ulb.info307.g6;
 
-import org.dizitart.no2.Nitrite;
-import org.dizitart.no2.objects.ObjectRepository;
+import ulb.info307.g6.controllers.CardDaoNitriteImplementation;
+import ulb.info307.g6.controllers.DeckDaoNitriteImplementation;
+import ulb.info307.g6.models.Card;
 import ulb.info307.g6.models.Deck;
 
 public class MainDB {
 
-    public static void gen_deck() {
-        Nitrite db = Nitrite.builder()
-                .compressed()
-                .filePath("db.db")
-                .openOrCreate();
-        ObjectRepository<Deck> collection = db.getRepository(Deck.class);
-        Deck p = new Deck("mdrlol");
-        collection.insert(p);
-        Deck p1 = new Deck("lolimodr");
-        collection.insert(p1);
-    }
-
     public static void main(String[] args) {
-        gen_deck();
-        Nitrite db = Nitrite.builder()
-                .compressed()
-                .filePath("db.db")
-                .openOrCreate();
-
-        ObjectRepository<Deck> collection = db.getRepository(Deck.class);
-
-       for (Deck p : collection.find()) {
-           System.out.println(p.getName());
-       }
+        DeckDaoNitriteImplementation ddni = new DeckDaoNitriteImplementation();
+        CardDaoNitriteImplementation cdni = new CardDaoNitriteImplementation();
+        for (Card c : cdni.getAllCards()) {
+            System.out.println(c.getQuestion());
+        }
+        for (Deck d : ddni.getAllDecks()) {
+            System.out.println(d.getName());
+        }
     }
 }
