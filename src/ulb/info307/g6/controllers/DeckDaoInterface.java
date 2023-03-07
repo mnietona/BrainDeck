9d796528f75bd003e33b1cbe6555b9eb50103f4a
Arthur Coenen <45007111+arthurco@users.noreply.github.com>
2023-03-07 23:04:36 +0100

    H2-T3 : added Deck database integration and tests for them.
    Co-authored-by: <Simon.Peetroons@ulb.be>

---
+++ src/ulb/info307/g6/controllers/DeckDaoInterface.java
@@ -0,0 +1,14 @@
+package ulb.info307.g6.controllers;
+
+import ulb.info307.g6.models.Card;
+import ulb.info307.g6.models.Deck;
+
+import java.util.List;
+
+public interface DeckDaoInterface {
+    public boolean addDeck(Deck deck);
+    public boolean deleteDeck(Deck deck);
+    public boolean updateDeck(Deck deck);
+    public List<Deck> getAllDecks();
+    public Deck getDeckById(long id);
+}
