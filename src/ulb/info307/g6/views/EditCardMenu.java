434f5d81a12a82c54fd18d7509f20217231ba946
Konrad <konrad.gracki@ulb.be>
2023-03-09 17:47:43 +0100

    H3-T1 : Adding view/EditCardMenu.java

---
+++ src/ulb/info307/g6/views/EditCardMenu.java
@@ -0,0 +1,57 @@
+package ulb.info307.g6.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class EditCardMenu {
+
+    @FXML
+    private Button addNewButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button question00;
+
+    @FXML
+    private Button question01;
+
+    @FXML
+    private Button question02;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    void addNewClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void questionClickHandler(ActionEvent event) {
+
+    }
+
+
+
+}
