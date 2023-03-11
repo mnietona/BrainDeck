c76a2db7ab6362870753c2c864fa398a1a3a49b7
Konrad <konrad.gracki@ulb.be>
2023-03-11 21:18:07 +0100

    H3-T1: Added inputs for text processing

---
+++ src/ulb/info307/g6/views/EditCardInputs.java
@@ -0,0 +1,34 @@
+
+package ulb.info307.g6.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+public class EditCardInputs {
+
+    @FXML
+    private TextArea answerInput;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextArea questionInput;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    void backClick(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void saveClick(ActionEvent event)
+    {
+
+    }
+
+}
