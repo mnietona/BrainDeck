5f6257ccf0569a28b254a28c20f58b8415cf3106
Simon Peetroons <Simon.Peetroons@ulb.be>
2023-03-13 16:29:57 +0100

    H4 - T1, T2 Backend + Frontend Étude de carte Implementation is similar to deck changes with a ComboBox
    Co-authored-by: Arnaud Querinjean <arnaud.querinjean@ulb.be>
    Co-authored-by: Co-Authored-By: Matias Nieto Navarrete <matias.nieto.navarrete@ulb.be>
    Co-authored-by: Alban Linchet <alban.linchet.martinez@ulb.be>

--- src/ulb/info307/g6/views/ChooseDeckPlay.java
+++ src/ulb/info307/g6/views/ChooseDeckPlay.java
@@ -6,24 +6,34 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.VBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
 import ulb.info307.g6.controllers.DeckDaoNitriteImplementation;
+import ulb.info307.g6.controllers.CardDaoNitriteImplementation;
 import ulb.info307.g6.models.Deck;
+import ulb.info307.g6.models.Card;
 
 import java.util.List;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import javafx.scene.text.Text;
+
+
+// TODO: Removes spaces before "Answer" to allign it in the middle
 
 public class ChooseDeckPlay {
     static DeckDaoNitriteImplementation database = new DeckDaoNitriteImplementation(); // Initialize the DAO for the database
+    static CardDaoNitriteImplementation databaseCard = new CardDaoNitriteImplementation(); // Initialize the DAO for the database
     public List<Deck> decks;
     public ChooseDeckPlay() {}
 
-    // definir
     @FXML
     private Button buttonHome;
     @FXML
@@ -39,19 +49,79 @@ public class ChooseDeckPlay {
     private ComboBox<Deck> cardPack;
 
     @FXML
-    private Rectangle cardRectangle;
+    private Text displayTitle;
+
+    @FXML
+    private Text displayTextQA;
+    @FXML
+    private ComboBox<String> knowledgeLevel;
+
+    public void setChoice() {
+        knowledgeLevel.getItems().clear();
+        ObservableList<String> options = FXCollections.observableArrayList("Very bad", "Bad", "Average", "Good", "Very good");
+        for (String option : options) {
+            knowledgeLevel.getItems().add(option);
+        }
+        knowledgeLevel.setOnAction(event -> { // click on an item
+            updateKnowledgeLevel();
+        });
+    }
+
+    @FXML
+    protected void clickKnowledgeLevel() {
+        Deck selectedItem = cardPack.getSelectionModel().getSelectedItem(); // Checks if a deck is currently selected
+        if (selectedItem != null) {
+            setChoice();
+        }
+    }
+
+    public void updateKnowledgeLevel() {
+        Deck selectedItem = cardPack.getSelectionModel().getSelectedItem();
+        Card card = selectedItem.getCardList().get(i);
+        if (card != null) {
+            if (knowledgeLevel.getValue() == "Very bad") {
+                card.setKnowledgeLevel(0);
+            } else if (knowledgeLevel.getValue() == "Bad") {
+                card.setKnowledgeLevel(1);
+            } else if (knowledgeLevel.getValue() == "Average") {
+                card.setKnowledgeLevel(2);
+            } else if (knowledgeLevel.getValue() == "Good") {
+                card.setKnowledgeLevel(3);
+            } else if (knowledgeLevel.getValue() == "Very good") {
+                card.setKnowledgeLevel(4);
+            }
+        }
+        databaseCard.updateCard(card);
+        database.updateDeck(selectedItem);
+    }
 
     @FXML
     protected void clickFlipCard() {
+        Deck selectedItem = cardPack.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            if (displayTitle.getText() == "Question") {
+                updateDisplayArea("  Answer");
+            } else if (displayTitle.getText() == "  Answer") {
+                updateDisplayArea("Question");
+            }
+        }
         System.out.println("Flip");
     }
     @FXML
     protected void clickNextCard() {
+        if (i <= decks.size()) {
+            i++;
+            updateDisplayArea("Question");
+        }
         System.out.println("Next");
 
     }
     @FXML
     protected void clickBackCard() {
+        if (i > 0) {
+            i--;
+            updateDisplayArea("Question");
+        }
         System.out.println("Back");
 
     }
@@ -59,7 +129,6 @@ public class ChooseDeckPlay {
     protected void clickChoice() {
         setCardPackLists();
     }
-
 
     public void clickHome() {
         accessNewWindow("/ulb/info307/g6/views/MainMenu.fxml");
@@ -72,33 +141,23 @@ public class ChooseDeckPlay {
             cardPack.getItems().add(deck);
         }
         cardPack.setOnAction(event -> { // click on an item
-            updateRectangleColor();
+            updateDisplayArea("Question");
         });
 
     }
 
-    public void updateRectangleColor() {
+    private int i = 0;
+    public void updateDisplayArea(String name) {
         Deck selectedItem = cardPack.getSelectionModel().getSelectedItem();
-
         if (selectedItem != null) {
-            switch (selectedItem.getName()) {
-                case "Math":
-                    cardRectangle.setFill(Color.RED);
-
-                    break;
-                case "Algo":
-                    cardRectangle.setFill(Color.GREEN);
-                    break;
-                case "Python":
-                    cardRectangle.setFill(Color.BLUE);
-                    break;
-                default:
-                    cardRectangle.setFill(Color.GRAY);
-                    break;
+            if (name == "Question") {
+                displayTitle.setText("Question");
+                displayTextQA.setText(selectedItem.getCardList().get(i).getQuestion());
+            } else if (name == "  Answer") {
+                displayTitle.setText("  Answer");
+                displayTextQA.setText(selectedItem.getCardList().get(i).getAnswer());
             }
         }
-
-
     }
 
     public void accessNewWindow(String name) {
@@ -114,6 +173,5 @@ public class ChooseDeckPlay {
             e.printStackTrace();
         }
     }
-
 }
 
