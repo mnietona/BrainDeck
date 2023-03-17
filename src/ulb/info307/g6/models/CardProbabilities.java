c0b5aa8d99c17d25aef595eec9f68fd4e935ea3e
Konrad <konrad.gracki@ulb.be>
2023-03-17 11:57:59 +0100

    H5-T1: added initCardProbabilities method
    Co-authored-by: tsua0002 <thomas.suau@ulb.be>

--- src/ulb/info307/g6/models/CardProbabilities.java
+++ src/ulb/info307/g6/models/CardProbabilities.java
@@ -2,4 +2,17 @@ package ulb.info307.g6.models;
 
 public class CardProbabilities
 {
+    public double[] cardProbabilities;
+
+    public void initCardProbabilities(int numberOfCards)
+    {
+        double probability = (double) 1/numberOfCards;
+        double[] initialProbabilities = new double[numberOfCards];
+
+        for (int i = 0; i < initialProbabilities.length; i++)
+        {
+            initialProbabilities[i] = probability;
+        }
+        this.cardProbabilities = initialProbabilities;
+    }
 }
