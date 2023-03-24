0c9420f0c4a32810e1c0c7290e130a22d0f16751
aquerinj <arnaud.querinjean@ulb.be>
2023-03-24 12:51:56 +0100

    Refactoring - t4 changement de MainMenu
    Co-Authored-By: Matias Nieto Navarrete <matias.nieto.navarrete@ulb.be>

--- src/ulb/info307/g6/controllers/MainController.java
+++ src/ulb/info307/g6/controllers/MainController.java
@@ -6,30 +6,100 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+import ulb.info307.g6.views.ChooseDeckPlay;
+import ulb.info307.g6.views.MainMenu;
+import ulb.info307.g6.views.MainMenu.*;
+
 import java.io.IOException;
 
 
-public class MainController extends Application {
+public class MainController extends Application implements MainMenuListener {
+
+
+    private Stage currentStage; // to store the current stage
 
     @Override
     public void start(Stage primaryStage) {
+        currentStage = primaryStage;
         try {
-            FXMLLoader loader = new FXMLLoader(MainController.class.getResource("/ulb/info307/g6/views/MainMenu.fxml"));
+            FXMLLoader loader = getMainMenu();
             Parent root = loader.load();
+            MainMenu mainMenu = loader.getController(); // to get the controller of the new window
+            mainMenu.setListener(this); // to set the main controller of the new window
             Scene scene = new Scene(root, 600, 408);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Menu");
-            primaryStage.show();
+            currentStage = new Stage(); // to set the current stage
+            currentStage.setScene(scene);
+            currentStage.setTitle("Menu");
+            currentStage.show();
 
         } catch (IOException e) {
             showErrorAlert();
         }
     }
 
+
+    @NotNull
+    private static FXMLLoader getMainMenu() {
+        return new FXMLLoader(MainController.class.getResource("/ulb/info307/g6/views/MainMenu.fxml"));
+    }
+
+    public void studyButtonAction() {
+        // Actions à effectuer lors du clic sur le bouton "Study"
+        currentStage.hide();
+        try {
+            FXMLLoader loader = getChooseDeckPlay();
+            Parent root = loader.load();
+            ChooseDeckPlay chooseDeckPlay = loader.getController(); // to get the controller of the new window
+            Scene scene = new Scene(root, 600, 408);
+            currentStage = new Stage(); // to set the current stage
+            currentStage.setScene(scene);
+            currentStage.setTitle("Study your decks");
+            currentStage.show();
+            chooseDeckPlay.showChoice();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @NotNull
+    private FXMLLoader getChooseDeckPlay() {
+        return new FXMLLoader(getClass().getResource("/ulb/info307/g6/views/ChooseDeckPlay.fxml"));
+    }
+
+    public void editButtonAction() {
+        currentStage.hide();
+        // Actions à effectuer lors du clic sur le bouton "Edit"
+        try {
+            FXMLLoader loader = getEditMenu();
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 600, 408);
+            currentStage = new Stage(); // to set the current stage
+            currentStage.setTitle("Edit your decks");
+            currentStage.setScene(scene);
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+    @NotNull
+    private FXMLLoader getEditMenu() {
+        return new FXMLLoader(getClass().getResource("/ulb/info307/g6/views/EditMenu.fxml"));
+    }
+
+
     private void showErrorAlert() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
     }
+
 
     public static void main(String[] args) {
         launch(args);
--- src/ulb/info307/g6/views/MainMenu.java
+++ src/ulb/info307/g6/views/MainMenu.java
@@ -1,13 +1,8 @@
 package ulb.info307.g6.views;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-
-import java.io.IOException;
+import ulb.info307.g6.controllers.MainController;
 
 public class MainMenu {
 
@@ -17,45 +12,28 @@ public class MainMenu {
     @FXML
     public Button editButton;
 
+    private MainMenuListener listener;
+
+
+    public void setListener(MainController listener) {
+        this.listener = listener;
+    }
+
     public void studyButtonAction() {
-        // Actions à effectuer lors du clic sur le bouton "Study"
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ulb/info307/g6/views/ChooseDeckPlay.fxml"));
-            Parent root = loader.load();
-            ChooseDeckPlay chooseDeckPlay = loader.getController(); // to get the controller of the new window
-            Scene scene = new Scene(root, 600, 408);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setTitle("Study your decks");
-            stage.show();
-            chooseDeckPlay.showChoice();
-            closeWindow();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        listener.studyButtonAction();
+
+
+
     }
 
     public void editButtonAction() {
-        // Actions à effectuer lors du clic sur le bouton "Edit"
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ulb/info307/g6/views/EditMenu.fxml"));
-            Parent root = loader.load();
-            // EditMenu editMenu = loader.getController(); // to get the controller of the new window
-            Scene scene = new Scene(root, 600, 408);
-            Stage stage = new Stage();
-            stage.setTitle("Edit your decks");
-            stage.setScene(scene);
-            stage.show();
-            closeWindow();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        listener.editButtonAction();
+
     }
 
-    public void closeWindow() {
-        // to close the window when a new window is call.
-        Stage stage = (Stage) editButton.getScene().getWindow();
-        stage.close();
+    public interface MainMenuListener {
+        void studyButtonAction();
+        void editButtonAction();
     }
 
 
