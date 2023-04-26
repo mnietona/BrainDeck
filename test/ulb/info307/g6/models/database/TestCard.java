ad654929f1f3c7ad9f368e919dcd4d7ed42dfd8d
aquerinj <arnaud.querinjean@ulb.be>
2023-04-26 12:07:29 +0200

    H0 - T5 - create TestCard Co-Authored-By: Matias Nieto Navarrete <matias.nieto.navarrete@ulb.be>

---
+++ test/ulb/info307/g6/models/database/TestCard.java
@@ -0,0 +1,43 @@
+package ulb.info307.g6.models.database;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import ulb.info307.g6.models.Card;
+
+public class TestCard {
+    private Card card;
+
+    @BeforeEach
+    public void setUp() {
+        card = new Card("Question?", "Answer.");
+    }
+
+    @Test
+    public void testGetQuestion() {
+        assertEquals("Question?", card.getQuestion());
+    }
+
+    @Test
+    public void testSetQuestion() {
+        card.setQuestion("New question?");
+        assertEquals("New question?", card.getQuestion());
+    }
+
+    @Test
+    public void testGetAnswer() {
+        assertEquals("Answer.", card.getAnswer());
+    }
+
+    @Test
+    public void testGetNthFlippedAnswer() {
+        assertEquals("Answer.", card.getNthFlippedAnswer(0));
+    }
+
+    @Test
+    public void testSetAnswer() {
+        card.setAnswer("New answer.");
+        assertEquals("New answer.", card.getAnswer());
+    }
+
+}
