44c46ff6f776544043c332f1f4d1dcbff05b8ad2
Konrad <konrad.gracki@ulb.be>
2023-04-27 13:48:07 +0200

    H18 : added createAchievements
    Co-authored-by: Arnaud Querinjean <arnaud.querinjean@ulb.be>

--- src/ulb/info307/g6/views/Achievements.java
+++ src/ulb/info307/g6/views/Achievements.java
@@ -17,13 +17,31 @@ public class Achievements implements View {
 
     @FXML
     private void initialize() {
-        List<Achievement> achievements = new ArrayList<>();
-        achievements.add(new Achievement("Achieve 3 cards in a row"));
-        // Ajouter d'autres achievements ici
+        List<Integer> achieved = new ArrayList<>();
+        achieved.add(1);
+        achieved.add(1);
+        achieved.add(0);
+        achieved.add(0);
+        achieved.add(0);
 
+        List<Achievement> achievements = createAchievements(achieved);
         ObservableList<Achievement> observableAchievements = FXCollections.observableArrayList(achievements);
         cardListView.setItems(observableAchievements);
     }
+
+    private List<Achievement> createAchievements(List<Integer> achieved)
+    {
+        List<Achievement> achievements = new ArrayList<>();
+        achievements.add(new Achievement( "["+achieved.get(0)+"/1] Novice: Achieve a perfect score on your first set of flashcards. " ));
+        achievements.add(new Achievement("["+achieved.get(1)+"/1] Master of Cards: Achieve a perfect score on all sets of flashcards."));
+        achievements.add(new Achievement("["+achieved.get(2)+"/1] Regular Player: Play the flashcard game every day for 3 consecutive days."));
+        achievements.add(new Achievement("["+achieved.get(3)+"/1] Explorer: Complete a set of flashcards that contains LaTeX formulas."));
+        achievements.add(new Achievement("["+achieved.get(4)+"/1] Regular Player: Play the flashcard game every day for 10 consecutive days."));
+        return achievements;
+    }
+
+
+
 
     @FXML
     private void clickBack() {
