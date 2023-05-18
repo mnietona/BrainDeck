af2348cf506f69e79258d435cb1d088ed2def1df
Simon Peetroons <Simon.Peetroons@ulb.be>
2023-05-18 18:54:29 +0200

    [H8-T4] Changes by Adrien
    Co-authored-by: Adrien Beslier <adrien.beslier@ulb.be>

--- src/ulb/info307/g6/views/Study.java
+++ src/ulb/info307/g6/views/Study.java
@@ -26,6 +26,8 @@ public class Study extends ViewWithDeckList {
     private Slider knowledgeLvlSlider;
     @FXML
     private Button buttonFlipCard, buttonNextCard;
+    @FXML
+    private ImageView flipButtonIcon;
     private StudyListener listener;
 
     @Override
@@ -123,6 +125,10 @@ public class Study extends ViewWithDeckList {
 
     public void setColorWebView() {
         cardWebView.setPageFill(Color.TRANSPARENT);
+    }
+
+    public void turnFlipIcon() {
+        flipButtonIcon.setRotate(flipButtonIcon.getRotate() + 180);
     }
 
     @Override
