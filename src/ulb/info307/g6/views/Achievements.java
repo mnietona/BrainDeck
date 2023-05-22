49f7191737e478e69b7faade20dcb8a136f9dadc
arthurco <45007111+arthurco@users.noreply.github.com>
2023-05-22 19:16:49 +0200

    H0-T8 : Fixed aligment of achievements with progress bars and checkmarks
    Co-authored-by: <alban.linchet.martinez@ulb.be>

--- src/ulb/info307/g6/views/Achievements.java
+++ src/ulb/info307/g6/views/Achievements.java
@@ -7,22 +7,24 @@ import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import ulb.info307.g6.models.Achievement;
 
 public class Achievements implements View {
-    private class AchievementHbox extends HBox {
-        AchievementHbox(Achievement a, int tier) {
+    private class AchievementGridPane extends GridPane {
+        AchievementGridPane(Achievement a, int tier) {
             super();
-            setAlignment(Pos.CENTER);
-            setSpacing(25.0);
             getStyleClass().add("achievement-box");
-            getChildren().add(new Label(a.getName()));
+            Label achievmentLabel = new Label(a.getName());
+
+            add(achievmentLabel, 0, 0);
+            HBox barAndChecksContainer = new HBox();
+            barAndChecksContainer.setAlignment(Pos.CENTER_LEFT);
+            barAndChecksContainer.setSpacing(0.0);
             ProgressBar achievementProgressBar = new ProgressBar();
             achievementProgressBar.setProgress(a.getProgress());
             achievementProgressBar.setPrefWidth(250.0);
-            getChildren().add(achievementProgressBar);
+            barAndChecksContainer.getChildren().add(achievementProgressBar);
             if (tier == 0) {
                 ImageView achievementImageView = new ImageView(new Image("/ulb/info307/g6/views/icons/checkmark.png"));
                 achievementImageView.setFitHeight(75);
@@ -30,15 +32,21 @@ public class Achievements implements View {
                 ColorAdjust colorAdjust = new ColorAdjust();
                 colorAdjust.setSaturation(-1); // Set saturation to -1 for full grayness
                 achievementImageView.setEffect(colorAdjust);
-                getChildren().add(achievementImageView);
+                barAndChecksContainer.getChildren().add(achievementImageView);
             } else {
                 for(int i=0;i<tier;i++){
                     ImageView achievementImageView = new ImageView(new Image("/ulb/info307/g6/views/icons/checkmark.png"));
                     achievementImageView.setFitHeight(75);
                     achievementImageView.setFitWidth(75);
-                    getChildren().add(achievementImageView);
+                    barAndChecksContainer.getChildren().add(achievementImageView);
                 }
             }
+            add(barAndChecksContainer,1, 0);
+            ColumnConstraints textColumn = new ColumnConstraints();
+            textColumn.setPercentWidth(50);
+            ColumnConstraints barAndChecksColumn = new ColumnConstraints();
+            barAndChecksColumn.setPercentWidth(50);
+            getColumnConstraints().addAll(textColumn, barAndChecksColumn);
         }
     }
     @FXML
@@ -51,7 +59,7 @@ public class Achievements implements View {
 
 
     public void addNewAchievement(Achievement a, int tier) {
-        achievementsVbox.getChildren().add(new AchievementHbox(a, tier));
+        achievementsVbox.getChildren().add(new AchievementGridPane(a, tier));
     }
 
     @Override
