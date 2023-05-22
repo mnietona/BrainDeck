fa8b0322cdbe7fcf99104c6d5a2cb36cdab91a96
arthurco <45007111+arthurco@users.noreply.github.com>
2023-05-22 23:15:21 +0200

    H0-T12 : Renamed launcher class
    Co-authored-by: <alban.linchet.martinez@ulb.be>

--- src/ulb/info307/g6/Launcher.java
+++ src/ulb/info307/g6/Launcher.java
@@ -1,8 +1,12 @@
 package ulb.info307.g6;
 
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ulb.info307.g6.controllers.MainController;
 
+@SpringBootApplication
 public class Launcher {
     public static void main(String[] args) {
-        MainController.main(args);}
+        Application.launch(MainController.class, args);
+    }
 }
